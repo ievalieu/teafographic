9931878c6dd5d01d051b8fd178d850efab7f6ac2
itsevalieu <itsevalieu@gmail.com>
2017-08-19 22:05:04 -0700

    Transfer footer mockup to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,14 @@ import React, { PropTypes, Component } from "react";
 
 class Footer extends Component {
 	render() {
-		<div>FOOTER</div>
+		<footer className="page-footer teal" style="margin: 0 0">
+			<div className="footer-copyright">
+				<div className="container">
+					© 2017 Eva Lieu
+					<a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+				</div>
+			</div>
+        </footer>
 	}
 };
 
