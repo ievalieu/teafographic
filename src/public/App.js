087df5d9a86bf59a829f8f41b2876eb7113f832b
itsevalieu <itsevalieu@gmail.com>
2017-08-21 22:49:05 -0700

    Import components in App

--- src/public/App.js
+++ src/public/App.js
@@ -1,5 +1,11 @@
 import React, { PropTypes, Component } from "react";
 
+import Header from '../components/Header';
+import Navbar from '../components/Navbar';
+import Main from '../components/Main';
+import Footer from '../components/Footer';
+import Sidebar from '../components/Sidebar';
+
 class App extends Component {
 	render() {
 		<div>App.js</div>
