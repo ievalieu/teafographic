9b716a379c2b3f82af58df6949d77a8d70ef4f2e
itsevalieu <itsevalieu@gmail.com>
2017-08-23 23:06:09 -0700

    Add content to Header, Main, and Navbar class

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,15 @@ import React, { PropTypes, Component } from "react";
 
 class Navbar extends Component {
 	render() {
-		<div>
-			<div>NAVBAR</div>
-		</div>
+		<section style="margin-bottom: 25px">
+			<form style="margin: 0 0">
+				<div class="teal input-field" style="margin: 0 0;">
+					<input id="search" value="cat" type="search" required style="margin: 0 0;"/>
+					<label class="label-icon" for="search"><i class="material-icons">search</i></label>
+					<i class="material-icons" style="height: 10px; width: 10px; padding: 5px 15px;">close</i>
+				</div>
+			</form>	
+		</section>
 	}
 };
 
