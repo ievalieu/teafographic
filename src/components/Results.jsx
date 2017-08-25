8b14ded908fdd1b1d2914b18ffe164f3ec992f1e
itsevalieu <itsevalieu@gmail.com>
2017-08-24 23:20:19 -0700

    Make Results component

---
+++ src/components/Results.jsx
@@ -0,0 +1,12 @@
+import React, { PropTypes, Component } from "react";
+
+class Results extends Component {
+	render() {
+		<li>
+			<div class="collapsible-header"><i class="material-icons">filter_drama</i>Jasmine Pearl</div>
+			<div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+		</li>
+	}
+};
+
+export default Results;
