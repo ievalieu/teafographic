0353f03fe52185ca1beadd7cdada63ab92a74f80
itsevalieu <itsevalieu@gmail.com>
2017-09-19 23:56:03 -0700

    Add div content to Hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React, { PropTypes, Component } from "react";
+
+class Hero extends Component {
+	
+	render() {
+		return(
+			<div className="container">
+				<img src="#" alt="#" />
+			</div>
+		);
+	}
+	
+}
+
+export default Hero;
+
+//slider images? button?
