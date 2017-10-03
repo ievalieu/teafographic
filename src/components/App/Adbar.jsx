8ce852ebc0b172d2891e2b6225ff6aab7a6d75b7
itsevalieu <itsevalieu@gmail.com>
2017-10-02 23:46:30 -0700

    Adding content to Adbar component

---
+++ src/components/App/Adbar.jsx
@@ -0,0 +1,15 @@
+import React, { PropTypes, Component } from "react";
+import "../../assets/css/App/adbar.css";
+
+class Adbar extends Component {
+	
+	render() {
+		return(
+			<div>
+				<p>Free shipping over $75!</p>
+			</div>
+		);
+	}
+};
+
+export default Adbar;
