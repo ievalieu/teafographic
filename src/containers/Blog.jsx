a009e0fc665157f997028a40ec5e83900c0eafb7
itsevalieu <itsevalieu@gmail.com>
2017-10-31 22:43:59 -0700

    Pseudo code for slideshow hero

--- src/containers/Blog.jsx
+++ src/containers/Blog.jsx
@@ -7,13 +7,13 @@ class Blog extends Component {
 			<main className="container">
 				<div>
 					<h1>Tea Blog Lifestyle</h1>
-
 				</div>
+
 				<div>
 					<h2>Blog</h2>
 					Newsfeed rows
 				</div>
-				<div>
+				<div className="sidebar">
 					<form>
 						<input placeholder="Search Articles"></input>
 					</form>
@@ -35,3 +35,12 @@ export default Blog;
 	//description
 	//image
 //Sidebar
+
+//function for slideshow?
+//function landingSlideshow (object) {
+//	object being separate divs
+// each div should hold an image, a title, a description, and an action
+// return the object div into a div for that hero section.
+// timed slideshow? 3 seconds?
+// should allow for click function, click on the next tabs to go to a specific slideshow
+//}
