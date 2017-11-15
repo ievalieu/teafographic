c430930f32f3e912557deb1833b7d91e693cd152
itsevalieu <itsevalieu@gmail.com>
2017-11-14 23:28:24 -0800

    Pseudo coding shop page

--- src/containers/Shop.jsx
+++ src/containers/Shop.jsx
@@ -6,6 +6,12 @@ class Shop extends Component {
 		return(
 			<main className="container">
 				Shop Page
+				Should have the following:
+				1. Sidebar with tags
+				2. Button to allow search opt
+				3. Picture item. Price. Name. 
+				4. Page arrows. 
+				5. Searchbar should be at the top.
 			</main>
 		);
 	}
